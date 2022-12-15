78792e2e781992b7f6edecb2b08b2f215c7f2dad
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-12-15 20:59:59 +0500

    config object validator

--- index.ts
+++ index.ts
@@ -6,6 +6,11 @@ const config = readConfigFile () as RandomsConfig;
 validateConfigObject (config);
 const fileMatchRegex = new RegExp (config.include);
 
+// TODOS:
+// - create output directory if it doesn't exist
+// - create output file if it doesn't exist
+// - import all files in output file
+
 readDirectory (config.target, (file)=>{
     if (fileMatchRegex.test (file.fileName))
     {
